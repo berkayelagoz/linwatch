7c0be956eb90c5d229e7d65ddfd2f49fe8226d0f
berkayelagoz <berkayelagoz@gmail.com>
2025-05-06 03:52:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -333,4 +333,11 @@ async def start_monitoring():
     asyncio.create_task(monitoring_loop())
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=443,
+        ssl_keyfile="/etc/letsencrypt/live/linwatch1.mooo.com/privkey.pem",
+        ssl_certfile="/etc/letsencrypt/live/linwatch1.mooo.com/fullchain.pem"
+    )
+
